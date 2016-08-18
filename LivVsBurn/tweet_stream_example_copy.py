78ac8bca1a87da6dc3cc0dd7946e2116b1c692fc
Deepak Gautam <deepakgautam@Deepaks-MacBook-Pro.local>
2016-08-18 16:37:24 -0700

    new files

---
+++ LivVsBurn/tweet_stream_example_copy.py
@@ -0,0 +1,56 @@
+"""
+Example Stream listener for twitter API.
+"""
+from __future__ import print_function
+import tweepy
+import time
+import cnfg
+
+
+class TweetListener(tweepy.StreamListener):
+
+    def __init__(self):
+        super().__init__(*args)
+        client = MongoClient()
+        # Use tweetdb database
+        self.db = client.tweetdb
+
+    def on_status(self, tweet):
+        print(tweet.text)
+        # If we need to save , we can put save command to save to mongodb
+
+    def on_data(self, data):
+        """This will be called each time we receive stream data"""
+        # Decode JSON
+        try:
+            datajson = json.loads(data)
+
+            # We only want to store tweets in English
+            if "lang" in datajson and datajson["lang"] == "en":
+                # Store tweet info into the cooltweets collection.
+                self.db.tweetdb.insert(datajson)
+        except Exception:
+            print("Error in parsing data {}".format(data))
+
+    def on_error(self, error_msg):
+        print('Error: {}'.format(error_msg))
+
+    def on_timeout(self):
+        print('Timed Out.  Might be rate-limited.  Introduce Delay in the process.  ')
+        time.sleep(10)
+
+
+config = cnfg.load(".ssh/twitter.cfg")
+
+auth = tweepy.OAuthHandler(config["consumer_key"],
+                           config["consumer_secret"])
+auth.set_access_token(config["access_token"],
+                      config["access_token_secret"])
+
+
+tweet_listener = TweetListener()
+tweet_stream = tweepy.Stream(auth = auth, listener=tweet_listener)
+
+tweet_stream.filter(track=['#lfc','@lfc','lfc', 'liverpool','klopp','BurLiv'
+    , '#BurLiv', '#LivBur','LivBur'
+    ,'@BurnleyOfficial','BurnleyOfficial','Burnley','#Bur'])
